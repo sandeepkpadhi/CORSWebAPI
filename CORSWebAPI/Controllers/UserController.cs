5e6566c4beeb7443b360294e408a423929b265f5
Sandeep Kumar Padhi <sandeep.kp@agilepoint.com>
2021-07-08 21:02:14 +0530

    Add project files.

---
+++ CORSWebAPI/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using CORSWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CORSWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetUser(string id)
+        {
+            UserProfile user = new UserProfile() { Id = "TESTID", Name = "TEST NAME" };
+            return Ok(user);
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] UserProfile user)
+        {
+            //Create user
+            return StatusCode(201);
+        }
+
+
+        [HttpPut]
+        public IActionResult UpdateUser([FromBody] UserProfile user)
+        {
+            //Update user
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(string id)
+        {
+            //Delete user
+            return Ok();
+        }
+
+    }
+}
